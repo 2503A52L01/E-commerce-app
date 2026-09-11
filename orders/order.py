5d4d7cae8e484c46ef856d43ae7f9d7f4bc4b1ee
2503A52L01 <2503a52l01@sru.edu.in>
2026-09-11 12:52:01 +0530

    Add order management

---
+++ orders/order.py
@@ -0,0 +1,17 @@
+orders = []
+
+def create_order(username, items):
+    order = {
+        "username": username,
+        "items": items,
+        "status": "Placed"
+    }
+
+    orders.append(order)
+    return order
+
+def get_orders(username):
+    return [
+        order for order in orders
+        if order["username"] == username
+    ]
