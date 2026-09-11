b70fd16fe123481eb5cec976dd5bf2cb0129406e
2503A52L01 <2503a52l01@sru.edu.in>
2026-09-11 12:49:19 +0530

    update shopping cart functionality

--- cart/cart.py
+++ cart/cart.py
@@ -11,4 +11,4 @@ class Cart:
             self.items.remove(product)
 
     def total(self):
-        return sum(item["price"] for item in self.items)
+        return sum(item["price"] for item in self.items)# cart total calculation implemented
